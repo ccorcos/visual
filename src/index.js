36a7e4a959814553ed2f6fad4605ac167981a88b
Chet Corcos <ccorcos@gmail.com>
2017-03-23 14:08:08 -0700

    jsondiffpatch with staltz's suggestion

--- src/index.js
+++ src/index.js
@@ -1,98 +1,100 @@
-import snabbdom from "snabbdom";
-import h from "snabbdom/h";
-import classModule from "snabbdom/modules/class";
-import propsModule from "snabbdom/modules/props";
-import styleModule from "snabbdom/modules/style";
-import eventModule from "snabbdom/modules/eventlisteners";
-import componentApi, {
-  ComponentElement,
-  componentModule,
-  statefulModule,
-  effectModule,
-  fetchModule
-} from "./componentapi";
+// import snabbdom from "snabbdom";
+// import h from "snabbdom/h";
+// import classModule from "snabbdom/modules/class";
+// import propsModule from "snabbdom/modules/props";
+// import styleModule from "snabbdom/modules/style";
+// import eventModule from "snabbdom/modules/eventlisteners";
+// import componentApi, {
+//   ComponentElement,
+//   componentModule,
+//   statefulModule,
+//   effectModule,
+//   fetchModule
+// } from "./componentapi";
+//
+// const patchView = snabbdom.init([
+//   classModule,
+//   propsModule,
+//   styleModule,
+//   eventModule
+// ]);
+//
+// const patchFetch = snabbdom.init([fetchModule]);
+//
+// const patchComponent = snabbdom.init(
+//   [
+//     componentModule,
+//     statefulModule,
+//     effectModule("view", patchView),
+//     effectModule("fetch", patchFetch)
+//   ],
+//   componentApi
+// );
+//
+// const Counter = () =>
+//   h("counter", {
+//     stateful: {
+//       init: () => 0,
+//       actions: {
+//         inc: (state, event) => state + 1,
+//         dec: (state, event) => state - 1
+//       }
+//     },
+//     view: ({ state, actions }, children) =>
+//       h("div", [
+//         h("button", { on: { click: actions.dec } }, "-"),
+//         h("span", state.toString()),
+//         h("button", { on: { click: actions.inc } }, "+")
+//       ]),
+//     // we can implement a hotkeys effect module later
+//     keys: ({ state, actions }, children) =>
+//       h("", {
+//         "=": actions.inc,
+//         "-": actions.dec
+//       })
+//   });
+//
+// function start(component) {
+//   const componentRootElement = new ComponentElement("");
+//   const componentRootVNode = patchComponent(componentRootElement, component);
+//
+//   const viewRootElement = document.createElement("div");
+//   document.body.appendChild(viewRootElement);
+//   const viewRootVNode = patchView(
+//     viewRootElement,
+//     componentRootVNode.effects.view
+//   );
+// }
+//
+// // no need to warm when we refactor the effects module TODO
+// // need to warm up children components so we have access to their effects
+// function warm(component) {
+//   const componentRootElement = new ComponentElement("");
+//   const componentRootVNode = patchComponent(componentRootElement, component);
+//   return componentRootVNode;
+// }
+//
+// // This works!
+// // start(Counter());
+//
+// // The .elm isnt there to rerender with for some reason
+// const TwoCounters = h(
+//   "TwoCounters",
+//   {
+//     view: (_, children) => h("div", children)
+//   },
+//   [warm(Counter()), warm(Counter())]
+// );
+// start(TwoCounters);
+//
+// // Examples:
+// // delta counters
+// // listOf
+// // undoable
+// // publish
+//
+// // TODO.
+// // - why do we need tow "warm" up each component. ideally we could just call the functions from within the view function when we need them
+// // -
 
-const patchView = snabbdom.init([
-  classModule,
-  propsModule,
-  styleModule,
-  eventModule
-]);
-
-const patchFetch = snabbdom.init([fetchModule]);
-
-const patchComponent = snabbdom.init(
-  [
-    componentModule,
-    statefulModule,
-    effectModule("view", patchView),
-    effectModule("fetch", patchFetch)
-  ],
-  componentApi
-);
-
-const Counter = () =>
-  h("counter", {
-    stateful: {
-      init: () => 0,
-      actions: {
-        inc: (state, event) => state + 1,
-        dec: (state, event) => state - 1
-      }
-    },
-    view: ({ state, actions }, children) =>
-      h("div", [
-        h("button", { on: { click: actions.dec } }, "-"),
-        h("span", state.toString()),
-        h("button", { on: { click: actions.inc } }, "+")
-      ]),
-    // we can implement a hotkeys effect module later
-    keys: ({ state, actions }, children) =>
-      h("", {
-        "=": actions.inc,
-        "-": actions.dec
-      })
-  });
-
-function start(component) {
-  const componentRootElement = new ComponentElement("");
-  const componentRootVNode = patchComponent(componentRootElement, component);
-
-  const viewRootElement = document.createElement("div");
-  document.body.appendChild(viewRootElement);
-  const viewRootVNode = patchView(
-    viewRootElement,
-    componentRootVNode.effects.view
-  );
-}
-
-// no need to warm when we refactor the effects module TODO
-// need to warm up children components so we have access to their effects
-function warm(component) {
-  const componentRootElement = new ComponentElement("");
-  const componentRootVNode = patchComponent(componentRootElement, component);
-  return componentRootVNode;
-}
-
-// This works!
-// start(Counter());
-
-// The .elm isnt there to rerender with for some reason
-const TwoCounters = h(
-  "TwoCounters",
-  {
-    view: (_, children) => h("div", children)
-  },
-  [warm(Counter()), warm(Counter())]
-);
-start(TwoCounters);
-
-// Examples:
-// delta counters
-// listOf
-// undoable
-// publish
-
-// TODO.
-// - why do we need tow "warm" up each component. ideally we could just call the functions from within the view function when we need them
-// -
+import "./staltz";
---
+++ src/staltz.js
@@ -0,0 +1,202 @@
+import snabbdom from "snabbdom";
+import classModule from "snabbdom/modules/class";
+import propsModule from "snabbdom/modules/props";
+import styleModule from "snabbdom/modules/style";
+import eventModule from "snabbdom/modules/eventlisteners";
+import h from "snabbdom/h";
+import { DiffPatcher } from "jsondiffpatch/src/diffpatcher";
+
+const patchv = snabbdom.init([
+  classModule,
+  propsModule,
+  styleModule,
+  eventModule
+]);
+
+const diffpatcher = new DiffPatcher({
+  objectHash: obj => obj.key
+});
+
+diffpatcher.processor.pipes.diff.before("trivial", context => {
+  // functions are ignored
+  if (
+    typeof context.left === "function" && typeof context.right === "function"
+  ) {
+    context.setResult(undefined).exit();
+    return;
+  }
+  if (
+    typeof context.left === "function" || typeof context.right === "function"
+  ) {
+    context.setResult(context.right).exit();
+    // throw new Error(
+    //   "you cannot add or remove functions. functions are aliased since they cannot be diffed."
+    // );
+  }
+  // lazy Thunks
+  if (context.left instanceof Thunk && context.right instanceof Thunk) {
+    if (context.left.equals(context.right)) {
+      context.setResult(undefined).exit();
+    } else {
+      context.setResult([context.left.run(), context.right.run()]).exit();
+    }
+    return;
+  }
+});
+
+class Thunk {
+  constructor(fn, ...args) {
+    this.fn = fn;
+    this.args = args;
+  }
+  equals(t2) {
+    const t1 = this;
+    if (t1.fn !== t2.fn) {
+      return false;
+    }
+    if (t1.args.length !== t2.args.length) {
+      return false;
+    }
+    for (let i = 0; i < t1.args.length; i++) {
+      if (t1.args[i] !== t2.args[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  run() {
+    if (this.result) {
+      return this.result;
+    } else {
+      this.result = this.fn(...this.args);
+      return this.result;
+    }
+  }
+}
+
+const thunk = (...args) => new Thunk(...args);
+
+class EventEmitter {
+  constructor() {
+    this.id = 0;
+    this.subscribers = {};
+  }
+  subscribe(key, fn) {
+    const id = this.id;
+    if (!this.subscribers[key]) {
+      this.subscribers[key] = {};
+    }
+    this.subscribers[key][id] = fn;
+    this.id++;
+    return [key, id];
+  }
+  stop([key, id]) {
+    delete this.subscribers[key][id];
+  }
+  emit(key, value) {
+    Object.keys(this.subscribers[key]).forEach(id =>
+      this.subscribers[key][id](value));
+    Object.keys(this.subscribers["*"] || []).forEach(id =>
+      this.subscribers["*"][id](value));
+  }
+  clear() {
+    this.subscribers = {};
+  }
+}
+const created = field => {
+  return field && field.length === 1;
+};
+
+const updated = field => {
+  return field && field.length === 2;
+};
+
+const destroyed = field => {
+  return field && field.length === 3;
+};
+
+const patchc = (prev = {}, next) => {
+  const delta = diffpatcher.diff(prev, next);
+  if (delta.type) {
+    if (created(delta.type)) {
+      // created
+      const node = {};
+      node.element = next;
+      node.update = new EventEmitter();
+      node.bag = {};
+      node.bag.state = next.stateful.init();
+      node.bag.actions = {};
+      // connect actions to the event emitter
+      Object.keys(next.stateful.actions).forEach(key => {
+        node.bag.actions[key] = value => node.update.emit(key, value);
+      });
+      // subscribe to events to update state
+      Object.keys(next.stateful.actions).forEach(key => {
+        node.update.subscribe(key, value => {
+          node.bag.state = next.stateful.actions[key](node.bag.state, value);
+        });
+      });
+      // patch the view and subscribe to updates
+      node.effects = {};
+      node.effects.view = patchv(prev.node.effects.view, next.view(node.bag));
+      node.update.subscribe("*", value => {
+        node.effects.view = patchv(node.effects.view, next.view(node.bag));
+      });
+      next.node = node;
+      return next;
+    } else if (destroyed(delta.type)) {
+      // destroyed
+      prev.node.update.clear();
+    } else {
+      // updated
+      // TODO.
+    }
+    // TODO.
+    // if (delta.children) {
+    //   // recurse
+    // }
+  } else {
+    // update node
+    console.error("this shouldnt happen");
+    // TODO.
+    // if (delta.children) {
+    //   // recurse
+    // }
+  }
+};
+
+const Counter = () => ({
+  // when components are the same type, the state carries over!
+  type: "counter",
+  stateful: {
+    init: () => 0,
+    actions: {
+      inc: (state, event) => state + 1,
+      dec: (state, event) => state - 1
+    }
+  },
+  view: ({ state, actions }, children) =>
+    h("div", [
+      h("button", { on: { click: actions.dec } }, "-"),
+      h("span", state.toString()),
+      h("button", { on: { click: actions.inc } }, "+")
+    ]),
+  keys: ({ state, actions }, children) => ({
+    "=": actions.inc,
+    "-": actions.dec
+  })
+});
+
+const div = document.createElement("div");
+document.body.appendChild(div);
+
+const rootc = patchc(
+  {
+    node: {
+      effects: {
+        view: div
+      }
+    }
+  },
+  Counter()
+);
